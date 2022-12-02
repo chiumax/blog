995d103d09b67edf581e7676698a400a3e8d5504
joe biden <notjoebiden@gmail.com>
2022-12-02 18:00:11 -0500

    new post

---
+++ content/posts/mixing-money-friends/ImageWrapper.js
@@ -0,0 +1,36 @@
+/* eslint react/no-unknown-property: 0 */
+/* eslint react/prefer-stateless-function: 0 */
+
+/**
+ * Spotify player iframe widget
+ *
+ * @author Alexander Wallin <office@alexanderwallin.com>
+ * @see https://developer.spotify.com/technologies/widgets/spotify-play-button/
+ */
+
+import * as React from "react";
+import "./style.css";
+import styled from "styled-components";
+
+function Wrapper({ children }) {
+  const Container = styled.div`
+    height: 435px;
+    width: 100%;
+    display: flex;
+    justify-content: center;
+    align-items: flex-start;
+    & > * {
+      height: 400px;
+      width: 400px;
+      margin: 0 !important;
+    }
+
+    & > * > * {
+      margin: 0 !important;
+    }
+  `;
+
+  return <Container>{children}</Container>;
+}
+
+export default Wrapper;
